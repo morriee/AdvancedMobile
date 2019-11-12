04da2e1b90eb725484534061c88fa1e61c4b8ee3
morriee <42960479+morriee@users.noreply.github.com>
2019-11-12 09:02:37 +0900

    Update RegisterActivity.java

--- LoginMangement/RegisterActivity.java
+++ LoginMangement/RegisterActivity.java
@@ -3,12 +3,18 @@ package com.example.papaya;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class RegisterActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_login);
+
+        EditText idText  = (EditText) findViewById(R.id.idText);
+        EditText passwordText  = (EditText) findViewById(R.id.passwordText);
+        TextView welcomeMessage = (TextView) findViewById(R.id.welcomeMessage);
     }
 }
