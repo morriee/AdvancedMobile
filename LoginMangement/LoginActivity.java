0aedefac62960af528bc4bc80d8b33b32a130fe5
morriee <42960479+morriee@users.noreply.github.com>
2019-11-12 09:03:38 +0900

    Update LoginActivity.java

--- LoginMangement/LoginActivity.java
+++ LoginMangement/LoginActivity.java
@@ -3,6 +3,8 @@ package com.example.papaya;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        EditText idText  = (EditText) findViewById(R.id.idText);
+        EditText passwordText  = (EditText) findViewById(R.id.passwordText);
+        TextView welcomeMessage = (TextView) findViewById(R.id.welcomeMessage);
     }
 }
