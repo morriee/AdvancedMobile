aa9192e93f6167eb96811ea42a1bd6f7f1367f54
morriee <42960479+morriee@users.noreply.github.com>
2019-11-17 21:37:38 +0900

    Add files via upload

---
+++ MemoDiary/SecreatDiaryList.java
@@ -0,0 +1,14 @@
+package com.example.papaya;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecreatDiaryList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_secreat_diary_list);
+    }
+}
