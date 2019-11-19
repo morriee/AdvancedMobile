1f868f2c6e44766e226a0b0d08da92e18d89fbbb
morriee <42960479+morriee@users.noreply.github.com>
2019-11-19 15:17:32 +0900

    Update MainActivity.java

--- Main/MainActivity.java
+++ Main/MainActivity.java
@@ -2,103 +2,331 @@ package com.example.papaya;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
-import android.view.View;
-
 import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.Calendar;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import android.widget.TextView;
+
+
+
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Date;
+import java.util.Locale;
+
+
+import org.jsoup.Jsoup;
+
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class MainActivity extends AppCompatActivity {
+    public  String ImagePath = null;
+    Bitmap bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void onClick(View view){
+        TextView times = (TextView)findViewById(R.id.Time);
 
-    }
 
-    //시계
-    @Override
-    protected void onStart(){
-        super.onStart();
-        Thread myThread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while (true) {
-                    try {
-                        handler.sendMessage(handler.obtainMessage());
-                        Thread.sleep(1000);
-                    } catch (Throwable t) {
+        Calendar cal = Calendar.getInstance();
+
+        int month = cal.get(Calendar.MONTH)+1;
+        int date = cal.get(Calendar.DATE);
+        int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
+
+        int hour = cal.get(Calendar.HOUR);
+        int minute = cal.get(Calendar.MINUTE);
+
+        SimpleDateFormat AmPm = new SimpleDateFormat("a", Locale.US);
+
+        Date today = new Date();
+        String AmPmString = AmPm.format(today);
+
+        String korDayOfWeek = null;
+        switch (dayOfWeek){
+            case 1:
+                korDayOfWeek = "SUN";
+                break;
+            case 2:
+                korDayOfWeek = "MON";
+                break;
+            case 3:
+                korDayOfWeek = "TUE";
+                break;
+            case 4:
+                korDayOfWeek = "WED";
+                break;
+            case 5:
+                korDayOfWeek = "THU";
+                break;
+            case 6:
+                korDayOfWeek = "FRI";
+                break;
+            case 7:
+                korDayOfWeek = "SAT";
+                break;
+            default:
+                break;
+        }
+        times.setText(month+"/"+date+"  "+korDayOfWeek+"  "+hour+":"+minute+"  "+AmPmString);
+
+
+        TextView textView = (TextView)findViewById(R.id.Temperature);
+        TextView textView1= (TextView)findViewById(R.id.Finedust);
+        TextView textView2= (TextView)findViewById(R.id.Ozone);
+        ImageView imageView= (ImageView)findViewById(R.id.weather_icon);
+
+
+
+
+
+
+
+        WeatherConnection weatherConnection = new WeatherConnection();
+        FinedustConnection finedustConnection = new FinedustConnection();
+        OzoneConnection ozoneConnection = new OzoneConnection();
+
+
+
+        AsyncTask<String, String, String> result1 = weatherConnection.execute("","");
+        AsyncTask<String, String, String> result2 = finedustConnection.execute("","");
+        AsyncTask<String, String, String> result3 = ozoneConnection.execute("","");
+
+        try{
+            String msg1 = result1.get();
+            String msg2 = result2.get();
+            String msg3 = result3.get();
+
+
+
+            if (msg1.matches(".*맑음*.")) {
+                ImagePath = "https://ssl.pstatic.net/static/weather/images/w_icon/w_l1.gif";
+
+            } else if (msg1.matches(".*구름조금*.")) {
+                ImagePath = "https://ssl.pstatic.net/static/weather/images/w_icon/w_l2.gif";
+
+            } else if (msg1.matches(".*흐림*.")) {
+                ImagePath = "https://ssl.pstatic.net/static/weather/images/w_icon/w_l3.gif";
+            } else if (msg1.matches(".*비*.")) {
+                ImagePath = "https://ssl.pstatic.net/static/weather/images/w_icon/w_l4.gif";
+
+            } else if (msg1.matches(".*눈*.")) {
+                ImagePath = "https://ssl.pstatic.net/static/weather/images/w_icon/w_l5.gif";
+
+            } else if (msg1.matches(".*진눈깨비*.")) {
+                ImagePath = "https://ssl.pstatic.net/static/weather/images/w_icon/w_l6.gif";
+
+            } else if (msg1.matches(".*소나기*.")) {
+                ImagePath = "https://ssl.pstatic.net/static/weather/images/w_icon/w_l7.gif";
+
+            } else if (msg1.matches(".*소낙눈*.")) {
+                ImagePath = "https://ssl.pstatic.net/static/weather/images/w_icon/w_l8.gif";
+
+            } else if (msg1.matches(".*안개*.")) {
+                ImagePath = "https://ssl.pstatic.net/static/weather/images/w_icon/w_l9.gif";
+
+            } else if (msg1.matches(".*뇌우*.")) {
+                ImagePath = "https://ssl.pstatic.net/static/weather/images/w_icon/w_l10.gif";
+
+            } else if (msg1.matches(".*흐려짐*.")) {
+                ImagePath = "https://ssl.pstatic.net/static/weather/images/w_icon/w_l11.gif";
+
+            } else if (msg1.matches(".*흐려져뇌우*.")) {
+                ImagePath = "https://ssl.pstatic.net/static/weather/images/w_icon/w_l12.gif";
+
+            } else if (msg1.matches(".*흐려져비*.")) {
+                ImagePath = "https://ssl.pstatic.net/static/weather/images/w_icon/w_l13.gif";
+
+            } else if (msg1.matches(".*흐려져눈*.")) {
+                ImagePath = "https://ssl.pstatic.net/static/weather/images/w_icon/w_l14.gif";
+
+            } else if (msg1.matches(".*흐려져진눈깨비*.")) {
+                ImagePath = "https://ssl.pstatic.net/static/weather/images/w_icon/w_l15.gif";
+
+            } else if (msg1.matches(".*흐린후갬*.")) {
+                ImagePath = "https://ssl.pstatic.net/static/weather/images/w_icon/w_l16.gif";
+
+            } else if (msg1.matches(".*뇌우후갬*.")) {
+                ImagePath = "https://ssl.pstatic.net/static/weather/images/w_icon/w_l17.gif";
+
+            } else if (msg1.matches(".*비후갬*.")) {
+                ImagePath = "https://ssl.pstatic.net/static/weather/images/w_icon/w_l18.gif";
+
+            } else if (msg1.matches(".*눈후갬*.")) {
+                ImagePath = "https://ssl.pstatic.net/static/weather/images/w_icon/w_l19.gif";
+
+            } else if (msg1.matches(".*진눈깨비후갬*.")) {
+                ImagePath = "https://ssl.pstatic.net/static/weather/images/w_icon/w_l20.gif";
+
+            } else if (msg1.matches(".*구름많음*.")) {
+                ImagePath = "https://ssl.pstatic.net/static/weather/images/w_icon/w_l21.gif";
+
+            } else if (msg1.matches(".*황사*.")) {
+                ImagePath = "https://ssl.pstatic.net/static/weather/images/w_icon/w_l22.gif";
+
+            } else
+                System.out.println("error");
+
+            textView.setText(msg1.toString());
+            textView1.setText("미세먼지   "+msg2.toString());
+            textView2.setText("오존          "+msg3.toString());
+
+            Thread mThread = new Thread(){
+                @Override
+                public void run(){
+                    try{
+                        URL url = new URL(ImagePath);
+                        HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+                        conn.setDoInput(true);
+                        conn.connect();;
+
+                        InputStream is = conn.getInputStream();
+                        bitmap = BitmapFactory.decodeStream(is);
+
+                    }catch(IOException ex) {
+
                     }
+
                 }
+            };
+            mThread.start();
+            try{
+                mThread.join();
+                imageView.setImageBitmap(bitmap);
+            } catch (InterruptedException e){
+
             }
-        });
-        myThread.start();
+
+
+        }catch (Exception e){
+
+        }
+
+
     }
 
-    Handler handler =new Handler(){
-        //@Override
-        public void handlerMessage(Message msg){ updateThread(); }
-    };
-
-    private void updateThread(){
-        String currentTime = currentTime();
-        text.setText(currentTime);
+    public void onClick(View v){
+        switch(v.getId()) {
+        }
     }
 
-    public String currentTime(){
-        long current = System.currentTimeMillis();      //현재 시간 저장
-        Date date = new Date(current);
 
-        SimpleDateFormat sdfHour = new SimpleDateFormat("HH");
-        String temp = sdfHour.format(date);
-        String ampm = "";
 
-        if(Integer.parseInt(temp) >= 12){
-            temp = String.valueOf(Integer.parseInt(temp)-12);
-            ampm =" PM";
-        }else {
-            ampm = "AM";
+    public class WeatherConnection extends AsyncTask<String, String, String>{
+
+
+        @Override
+        protected String doInBackground(String... params) {
+
+
+
+            try{
+
+                String path = "https://weather.naver.com/rgn/townWetr.nhn?naverRgnCd=09290555";
+
+                Document document = Jsoup.connect(path).get();
+
+                Elements elements = document.select("em");
+
+                System.out.println(elements);
+
+                Element targetElement = elements.get(2);
+
+                String text = targetElement.text();
+
+                System.out.println(text);
+
+                return text;
+
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+            return null;
         }
-        SimpleDateFormat sdfMin = new SimpleDateFormat("mm");
-        String temp2 = sdfMin.format(date);
-        temp = temp + ":" + temp2 +ampm;
+    }
 
-        SimpleDateFormat sdfMon = new SimpleDateFormat("MM/DD");
-        SimpleDateFormat sdfDate = new SimpleDateFormat("D");
-        String month = sdfMon.format(date);
-        String d = sdfDate.format(date);
+    public class FinedustConnection extends AsyncTask<String, String, String>{
 
-        switch(d){
-            case "월":
-                d = " Mon ";
-                break;
-            case "화":
-                d = " Tue ";
-                break;
-            case "수":
-                d = " Wed ";
-                break;
-            case "목":
-                d = " Thu ";
-                break;
-            case "금":
-                d = " Fri ";
-                break;
-            case "토":
-                d = " Sat ";
-                break;
-            case "일":
-                d = " Sun ";
-                break;
+
+        @Override
+        protected String doInBackground(String... params) {
+
+
+
+            try{
+
+                String path = "https://weather.naver.com/rgn/townWetr.nhn?naverRgnCd=09290555";
+
+                Document document = Jsoup.connect(path).get();
+
+                Elements elements = document.select("em");
+
+                System.out.println(elements);
+
+                Element targetElement = elements.get(3);
+
+                String text = targetElement.text();
+
+                System.out.println(text);
+
+                return text;
+
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+            return null;
         }
-        month = month + d + temp;
-        return month;
+    }
+
+    public class OzoneConnection extends AsyncTask<String, String, String>{
+
+
+        @Override
+        protected String doInBackground(String... params) {
+
+
+            try{
+
+                String path = "https://weather.naver.com/rgn/townWetr.nhn?naverRgnCd=09290555";
+
+                Document document = Jsoup.connect(path).get();
+
+                Elements elements = document.select("em");
+
+                System.out.println(elements);
+
+                Element targetElement = elements.get(4);
+
+                String text = targetElement.text();
+
+                System.out.println(text);
+
+                return text;
+
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+            return null;
+        }
     }
 }
